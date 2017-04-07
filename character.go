a6daa04e0fab498897e2c41950fae0fd746c0193
wuqunlin <wuqunlin@g7.com.cn>
2017-04-07 14:16:55 +0800

    add three func

---
+++ character.go
@@ -0,0 +1,38 @@
+package gozero
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+// GbkToUtf8 convert gbk to utf8
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+// Utf8ToGbk convert utf8 to gbk
+func Utf8ToGbk(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+// StrToBin convert to string to bin string
+func StrToBin(src string) (binString string) {
+	for _, c := range src {
+		binString = fmt.Sprintf("%s%b", binString, c)
+	}
+	return
+}
